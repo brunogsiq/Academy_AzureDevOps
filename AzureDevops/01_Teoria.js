df193147da3b2cf50c392769c41630fcf41fd4e8
brunogsiq <brunogsiq@gmail.com>
2025-01-20 10:12:36 -0300

    New project

---
+++ AzureDevops/01_Teoria.js
@@ -0,0 +1,83 @@
+/* 
+    https://www.udemy.com/course/curso-oficial-para-certificacao-exin-devops-profissional/learn/lecture/13141180?start=0#overview
+
+    Conteitos.
+        1º Conceito
+            Cultura colaborativa
+                Não existe uma cultura Devops sem colaboração.
+            Facilitação:
+            Transparência:
+            Confiança:
+
+        2º Conceito
+            Integração contínua:
+                Realizar, em tese, de maneira automatizada a integração
+            Entrega contínua:
+
+            Implantação contínua:
+
+        3º Conceito
+            Microserviços:
+            Apis:
+            Infraestrutura como código:
+            Releases de baixo risco:
+            Injeção de falhas:
+
+        4º Conceito:
+            Pipeline de implantação:
+            Automação de Testes:
+            TDD:
+
+        5º Conceito:
+            Revisão de código:
+            Programação em pares:
+            Gestão de mudanças:
+            Segurança contínua:
+
+        6º Conceito:
+            Telemetria:
+            Experimentação:
+            Feedback contínuo:
+            Testes A/B
+
+        7º Conceito:
+            Lean:
+            Scrum:
+            Definição de pronto:
+            Kanban:
+            XP:
+            Kata:
+            Teoria das restrições:
+
+    Devops 
+        É uma sigla que significa duas palavras: Desenvolvimento e Operações (Development + Operations)
+
+    Lean: 
+        Criado pela Toyota.
+        Filosofia que respeitar as pessoas, são Elas que fazem a diferença.
+        Valor para o Cliente, eliminando desperdício, ou seja, relatório realizado sem solicitação.
+        Foco na causa dos problemas, ao invés de apontar para Pessoas.
+
+        Princípios do Lean:
+            Valor na visão do cliente.
+            Fluxo de valor e mapeamento.
+            Fluxo contínuo sem interrupção.
+            Produção puxada.
+            Melhoria contínua e perfeição.
+            Gemba, significa o local real onde as coisas estão acontecendo.
+
+        Manifesto Ágil:
+            Individuos e interações mais que processos e ferramentas
+            Software em funcionamento mais que documentação abrangente
+            Colaboração com o cliente mais que a negociação de contratos
+            Responder a mudanças mais que seguir um plano
+
+        12 Princípios do Manifesto Ágil:
+            A maior prioridade do time é satisfazer o cliente. através da entrega adiantada e contínua do software de valor.
+            Aceitar mudanças de requisitos, mesmo no fim do desenvolvimernto. Processos ágeis se adequam a mudanças, para que o cliente possa tirar vantagens competitivas.
+            Entregar software funcionando com frequência, na escala de semanas até meses, com preferência aos períodos mais curtos.
+            Pessoas relacionadas à negócios e desenvolvedores devem trabalhar em conjunto diariamente, durante todo o curso do projeto.
+            Construir projetos ao redor de indivíduos motivados, dando a Eles o ambiente e suporte necessário, e confiar que farão seu trabalho.
+            O metódo mais eficiente e eficaz de transmitir informações para, e por dentro de um time de desenvolvimento, e através de uma conversa cara a cara.
+
+*/
